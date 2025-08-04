cc0aa5415996f29b934dab9afcf3adf078eaf0a4
ChrisBeardsmore <chris.beardsmore@dyce-energy.co.uk>
2025-08-04 10:56:23 +0100

    Update base_rate_lookup.py

--- apps/directgas/logic/base_rate_lookup.py
+++ apps/directgas/logic/base_rate_lookup.py
@@ -1,1 +1,14 @@
+def get_base_rates(ldz, kwh, duration, carbon_offset_required, flat_df):
+    match = flat_df[
+        (flat_df["LDZ"] == ldz) &
+        (flat_df["Contract_Duration"] == duration) &
+        (flat_df["Minimum_Annual_Consumption"] <= kwh) &
+        (flat_df["Maximum_Annual_Consumption"] >= kwh) &
+        (flat_df["Carbon_Offset"] == carbon_offset_required)
+    ]
 
+    if not match.empty:
+        best = match.sort_values("Unit_Rate").iloc[0]
+        return round(best["Standing_Charge"], 2), round(best["Unit_Rate"], 3)
+    else:
+        return 0.0, 0.0
