b1845d2e9922d52d2b791eb53240469b7b6d3a31
ChrisBeardsmore <chris.beardsmore@dyce-energy.co.uk>
2025-08-05 12:28:13 +0100

    Update flat_file_loader.py

--- apps/directgas/logic/flat_file_loader.py
+++ apps/directgas/logic/flat_file_loader.py
@@ -32,4 +32,5 @@ def load_flat_file(uploaded_file) -> pd.DataFrame:
     df["Contract_Duration"] = pd.to_numeric(df["Contract_Duration"], errors='coerce').fillna(0).astype(int)
     df["Minimum_Annual_Consumption"] = pd.to_numeric(df["Minimum_Annual_Consumption"], errors='coerce').fillna(0)
     df["Maximum_Annual_Consumption"] = pd.to_numeric(df["Maximum_Annual_Consumption"], errors='coerce').fillna(0)
-
+    
+    return df  # ADD THIS LINE!
