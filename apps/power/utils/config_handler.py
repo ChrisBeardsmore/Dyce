f1db3a73f44e38a68efaddbc814bd9eb87efa146
ChrisBeardsmore <chris.beardsmore@dyce-energy.co.uk>
2025-08-06 12:30:47 +0100

    Update config_handler.py

--- apps/power/utils/config_handler.py
+++ apps/power/utils/config_handler.py
@@ -1,8 +1,5 @@
+# utils/config_handler.py
 import json
-
-def save_uplift_config_dict(config_dict, file_path):
-    with open(file_path, "w") as f:
-        json.dump(config_dict, f, indent=2)
 
 def load_uplift_config(uploaded_file):
     return json.load(uploaded_file)
